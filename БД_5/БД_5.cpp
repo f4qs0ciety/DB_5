4d68e44b63bdac175496f65477f40606f40db173
Егор Будюкин <wickedshaman@protonmail.com>
2022-04-06 10:13:13 +0300

    Добавлены классы Network и Node

--- БД_5/БД_5.cpp
+++ БД_5/БД_5.cpp
@@ -1,20 +1,27 @@
-﻿// БД_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+class Network {
+private:
+	class Node {
+		int data;
+		Node* In1; Node* In2; Node* In3; //Входы узла
+		Node* Out1; Node* Out2; Node* Out3; Node* Out4; Node* Out5; //Выходы узла
+		Node(int input, Node* In1 = nullptr, Node* In2 = nullptr, Node* In3 = nullptr, Node* Out1 = nullptr, Node* Out2 = nullptr, Node* Out3 = nullptr, Node* Out4 = nullptr, Node* Out5 = nullptr) {
+			this->data = input;
+			this->In1 = In1;
+			this->In2 = In2;
+			this->In3 = In3;
+			this->Out1 = Out1;
+			this->Out2 = Out2;
+			this->Out3 = Out3;
+			this->Out4 = Out4;
+			this->Out5 = Out5;
+		}
+		Node* core = new Node(NULL);
+	};
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void main() {
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
